8d01edd4f1e19c614921e3ac28fbdd8b41c3c3c2
polygonplanet <polygon.planet.aqua@gmail.com>
2016-03-31 11:41:32 +0900

    Improve highlight for large file

--- HighlightAnything.py
+++ HighlightAnything.py
@@ -4,37 +4,70 @@ import sublime_plugin
 REGION_KEY = "HighlightAnything_Highlight"
 SETTINGS_KEY = "HighlightAnything.sublime-settings"
 
+DEFAULT_ENABLED = True
+DEFAULT_REGEXP = "[\u3000]+"
+DEFAULT_MAX_FILE_SIZE = 1048576
+DEFAULT_DELAY = 200
+
 st3 = True if int(sublime.version()) >= 3000 else False
 
-class HighlightAnything(sublime_plugin.EventListener):
 
-  def highlight(self, view):
-    regexp = self.load_settings().get("highlight_anything_regexp")
-    if regexp:
-      view.erase_regions(REGION_KEY)
-      if st3:
-        view.add_regions(REGION_KEY, view.find_all(regexp), "invalid", "", sublime.DRAW_OUTLINED)
-      else:
-        view.add_regions(REGION_KEY, view.find_all(regexp), "invalid", sublime.DRAW_OUTLINED)
+class Preferences:
+  def load(self, settings):
+    self.enabled = bool(settings.get("highlight_anything_enabled", DEFAULT_ENABLED))
+    self.regexp = settings.get("highlight_anything_regexp", DEFAULT_REGEXP)
+    self.max_size = settings.get("highlight_anything_max_file_size", DEFAULT_MAX_FILE_SIZE)
+    self.delay = settings.get("highlight_anything_delay", DEFAULT_DELAY)
+
+Pref = Preferences()
+
+
+def plugin_loaded():
+  settings = sublime.load_settings(SETTINGS_KEY)
+  Pref.load(settings)
+  settings.add_on_change("reload", lambda: Pref.load(settings))
+
+
+def highlight(view):
+  if view.size() > Pref.max_size:
+    return
+
+  if not Pref.regexp:
+    return
+
+  view.erase_regions(REGION_KEY)
+  if st3:
+    view.add_regions(REGION_KEY, view.find_all(Pref.regexp), "invalid", "", sublime.DRAW_OUTLINED)
+  else:
+    view.add_regions(REGION_KEY, view.find_all(Pref.regexp), "invalid", sublime.DRAW_OUTLINED)
+
+
+class HighlightAnything(sublime_plugin.EventListener):
+  def __init__(self):
+    self.pending = 0
 
   def render(self, view):
-    enabled = self.load_settings().get("highlight_anything_enabled")
-    if enabled:
-      self.highlight(view)
-    else:
-      view.erase_regions(REGION_KEY)
+    self.pending = self.pending - 1
+    if self.pending > 0:
+      return
+    highlight(view)
 
-  def load_settings(self):
-    return sublime.load_settings(SETTINGS_KEY)
-
-  # Called when the file is finished loading.
-  def on_load(self, view):
-    self.render(view)
-
-  # Called after changes have been made to a view.
   def on_modified(self, view):
-    self.render(view)
+    if Pref.enabled:
+      self.pending = self.pending + 1
+      if self.pending == 1:
+        sublime.set_timeout(lambda: self.render(view), 1)
+      else:
+        sublime.set_timeout(lambda: self.render(view), Pref.delay)
 
-  # Called when a view gains input focus.
   def on_activated(self, view):
-    self.render(view)
+    if Pref.enabled:
+      highlight(view)
+
+  def on_load(self, view):
+    if Pref.enabled:
+      highlight(view)
+
+
+if not st3:
+  plugin_loaded()
